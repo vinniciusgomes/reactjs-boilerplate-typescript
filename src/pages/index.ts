4b171e6e0b800ce0b88ec751e275cc64ac243a5a
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2021-03-15 10:43:39 -0300

    Clean files

--- src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-import SignIn from './Auth/SignIn';
-import Dashboard from './Dashboard';
-
-export default { Dashboard, SignIn };
--- src/routes/interfaces.ts
+++
@@ -1,6 +0,0 @@
-import { RouteProps as ReactDOMRouteProps } from 'react-router-dom';
-
-export interface IRouteProps extends ReactDOMRouteProps {
-  isPrivate?: boolean;
-  component: React.ComponentType;
-}
