4e1ef672a71536a20755d40fd5577f44ab878e0a
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2021-06-12 15:20:36 -0300

    Update theme.ts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,35 +1,35 @@
-import { lighten } from 'polished';
-import styled from 'styled-components';
-import theme from 'styled-theming';
+import { lighten } from 'polished'
+import styled from 'styled-components'
+import theme from 'styled-theming'
 
 export const backgroundColor = theme('mode', {
   light: '#fafafa',
-  dark: '#222',
-});
+  dark: '#121214',
+})
 
 export const textColorPrimary = theme('mode', {
   light: '#050505',
   dark: '#fff',
-});
+})
 
 export const textColorSecundary = theme('mode', {
   light: '#fff',
   dark: '#050505',
-});
+})
 
 export const buttonBackgroundColor = theme('mode', {
   light: lighten(0.2, '#5B3E96'),
   dark: '#5B3E96',
-});
+})
 
 export const buttonTextColor = theme('mode', {
   light: '#eee',
   dark: '#eee',
-});
+})
 
 export const Wrapper = styled.div`
   background-color: ${backgroundColor};
   color: ${textColorPrimary};
 
   font-size: 1.6rem;
-`;
+`
